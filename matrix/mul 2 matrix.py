86c0d1c9f300f3e0c452690f21dcc8e5fe4afdea
Manish Bhati <manishrinkubhati@gmail.com>
2024-07-31 13:47:04 +0530

    Day 7

--- matrix/mul 2 matrix.py
+++ matrix/mul 2 matrix.py
@@ -2,8 +2,9 @@ l1=[[1,2],[4,5]]
 l2=[[11,12],[14,15]]
 l3=[[0,0],[0,0]]
 for i in range(len(l1)):
-    
     for j in range(len(l2[0])):
-        l3[i][j] = l1[i][j]*l2[j][i]+l1[i][j+1]*l1[j][i]
-    print("\n",l3[i])
+        for k in range(len(l2)):
+            
+            l3[i][j] += l1[i][k]*l2[k][j]
+    print(l3[i])
 
