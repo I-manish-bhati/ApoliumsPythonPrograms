c2aa980834abfdd708d15b4ac8c77c93f3770268
Manish Bhati <manishrinkubhati@gmail.com>
2024-08-09 20:10:53 +0530

    Done

--- Dictionary/list of dictionary.py
+++ Dictionary/list of dictionary.py
@@ -6,7 +6,7 @@ mainl=[{
             "Name":"Don3",
             "Year":2025,
             "Director":"Navmann",
-            "Actor":["SRK","Salman","Manish"]
+            "Actor":["SRK","Salman","Manish","Amitabh","Dhanush"]
             }]
        },
        {
@@ -16,7 +16,7 @@ mainl=[{
         "Actor":["Nikunj","Navmann",'Anil',"Sonakshi",{"Name":"Don4",
             "Year":2029,
             "Director":"Nikunj",
-            "Actor":["SRK","Ayush","Manish","Salman"]}]
+            "Actor":["SRK","Ayush","Manish","Salman","Kabir","Manthan"]}]
        },
        {
         "Name":"Good news"  , 
@@ -25,7 +25,7 @@ mainl=[{
         "Actor":["Nikunj","Navmann","Salman","Sonakshi",{"Name":"Don5",
             "Year":2029,
             "Director":"Sejal",
-            "Actor":["SRK","Navmann","Manish"]}]
+            "Actor":["SRK","Navmann","Manish","Akshay","Rajveer"]}]
        },
        {
         "Name":"Gangs of Wassepur"  , 
@@ -43,12 +43,22 @@ mainl=[{
         "Actor":["Salman","Navmann","anil","Ishaan",{"Name":"Mars",
             "Year":2029,
             "Director":"Aamir khan",
-            "Actor":["SRK","Ayush","Manish"]}]
+            "Actor":["SRK","Ayush","Manish","Arav","Akshay"]}]
        }
        ]
+flag=0
 for i in range(len(mainl)):
     #mainl[i]["Actor"]
     #print(mainl[i]["Actor"][4]["Actor"][1])
-    if "Salman" in mainl[i]["Actor"] or "Salman" in mainl[i]["Actor"][4]["Actor"]:
+    """if "Salman" in mainl[i]["Actor"] or "Salman" in mainl[i]["Actor"][4]["Actor"]:
         if mainl[i]["Year"]>2012 and mainl[i]["Year"]<=2015 or mainl[i]["Actor"][4]["Year"]>2012 and mainl[i]["Actor"][4]["Year"]<=2015:
-            print(mainl[i]["Name"])
+            print(mainl[i]["Name"])"""
+            
+    for j in range(len(mainl[i]["Actor"])):
+               
+        if mainl[i]["Actor"][j]=="Salman" or mainl[i]["Actor"][4]["Actor"][j]:
+            if mainl[i]["Year"]>2012 and mainl[i]["Year"]<2015 or mainl[i]["Actor"][4]["Year"]>2012 and mainl[i]["Actor"][4]["Year"]<2015:
+                flag=1
+    if flag==1:
+        print(mainl[i]["Name"])
+    
