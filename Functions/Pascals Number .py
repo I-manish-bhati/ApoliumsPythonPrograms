1bf7b1e7cdc91b8f013bddace351bdb7f7be633a
Manish Bhati <manishrinkubhati@gmail.com>
2024-08-15 22:34:03 +0530

    Done

--- Functions/Pascals Number .py
+++ Functions/Pascals Number .py
@@ -1,13 +1,15 @@
+from math import factorial
+
 def pas(x):
-    print("Hello")
-    a=[]
-    sum=0
-    for i in range(x):
+    for i in range(x+1):
+        for j in range(x-i+1):
+            print(" ",end="")
+        for k in range(i+1):
+            #nCr= n!//(n-r)!*r*
+            print(factorial(i)//(factorial(i-k)*factorial(k)),end="  ")
+        print()
         
-            
-        
-            
-
-
-x=int(input("Enter number : "))
+    
+x=int(input("Enter no. of terms : "))
 pas(x)
+
